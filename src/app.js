0f468f112c8a7257139a8206cf91c7f26d74385f
Shubh Negi <shubhnegi0010>
2023-12-15 23:33:47 +0530

    code restructure

--- src/app.js
+++ src/app.js
@@ -4,14 +4,14 @@ import Header from './components/Header';
 import Body from './components/Body';
 
 const AppLayout = () => {
-    return (
-        <div className="app">
-            <Header/>
-            <Body/>
-        </div>
-    );
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout/>);
+root.render(<AppLayout />);
